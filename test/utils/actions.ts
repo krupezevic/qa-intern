5d8322195c1ba05fd9a7c43a820a707ae50d7787
Darko Anojcic <darko.anojcic@gmail.com>
2022-11-06 17:31:28 +0100

    initial commit

---
+++ test/utils/actions.ts
@@ -0,0 +1,21 @@
+class Actions {
+  public async typeIn(element, value) {
+    await this.waitForElementToBeDisplayed(element);
+    await element.doubleClick();
+    await browser.keys('Delete');
+    await element.setValue(value);
+  }
+
+  public async clickOn(element) {
+    await this.waitForElementToBeDisplayed(element);
+    await element.click();
+  }
+
+  public async waitForElementToBeDisplayed(element) {
+    await element.waitForExist();
+    await element.waitForDisplayed();
+    await element.waitForClickable();
+  }
+
+}
+export default new Actions();
